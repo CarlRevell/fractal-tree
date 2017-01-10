cf49afe4171d5dcfb6f284dbf0337026f47bc811
Carl Revell <carl@carlsworld.co.uk>
2017-01-10 22:43:27 +0000

    tidy up

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 (function() {
-  var angleControl, angleText, canvas, context, lengthControl, lengthText, growthControl, growthText;
+  var canvas, context, controls = {}, texts = {};
 
   window.addEventListener('load', load);
-  function load() {    
-    angleControl = document.getElementById('angle');
-    angleControl.min = 0;
-    angleControl.max = Math.PI / 4;
-    angleControl.step = Math.PI / 360;
-    angleText = document.getElementById('angleText');
-    lengthControl = document.getElementById('length');
-    lengthText = document.getElementById('lengthText');
-    growthControl = document.getElementById('growth');
-    growthText = document.getElementById('growthText');    
+
+  function load() {       
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
     context.fillStyle = 'white';
     context.strokeStyle = 'hsl(30, 50%, 20%)';
     context.lineCap = 'round';
-    angleControl.addEventListener('input', draw);
-    lengthControl.addEventListener('input', draw);
-    growthControl.addEventListener('input', draw);
-    Array.from(document.querySelectorAll['input[type=range]']).forEach(input => input.addEventListener('input', draw));
+
+    Array.from(document.querySelectorAll('input[type="range"]')).forEach(input => {
+      controls[input.id] = input;
+      texts[input.id] = document.getElementById(input.id + 'Text');
+      texts[input.id].textContent = input.value;
+      input.addEventListener('input', draw);
+    });
+
+    controls.angle.min = 0;
+    controls.angle.max = Math.PI / 4;
+    controls.angle.step = Math.PI / 360;
+
     draw();
   }
 
   function draw(e) {
     console.log(e, this);
     if (e) {
-      document.getElementById(this.id + 'Text').textContent = this.value;
+      texts[this.id].textContent = this.value;
     }
     
     // context.save();    
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.translate(canvas.width / 2, canvas.height);
-    branch(lengthControl.value, angleControl.value, 1);
+    branch(controls.length.value, controls.angle.value, 1);
     context.setTransform(1, 0, 0, 1, 0, 0);
     // context.restore();
   }
@@ -43,7 +43,7 @@
   }
 
   function branch(length, angle, evo) {
-    var growthFactor = parseFloat(growthControl.value)
+    var growthFactor = controls.growth.value
       , lineWidth = Math.max((18 - 2 *evo) + lerp(-0.5, 0.5), 1)
       , ran;
     
